8a662cb0ce2890af3942861b2afa73851de555f2
Mercury <>
2022-12-12 19:16:28 +0200

    Deck and pile

--- Assets/_Project/AppUI/Deck/Scripts/DeckManager.cs
+++ Assets/_Project/AppUI/Deck/Scripts/DeckManager.cs
@@ -1,14 +1,28 @@
 using _Project.AppUI.Components.Draggable.Scripts;
 using _Project.Core.Dealer.Interfaces;
+using _Project.Game;
 using UnityEngine;
 
 namespace _Project.AppUI.Deck.Scripts {
     [RequireComponent(typeof(DraggableContainerBase))]
     public class DeckManager : MonoBehaviour {
-
         [SerializeField] PileHandler _pile;
 
         [SerializeField] DiscardedPileHandler _discarded;
+
+        [SerializeField] GameManager _gameManager;
+
+        void OnEnable() {
+            _gameManager.OnDeckDealt += OnDeckDealt;
+        }
+
+        void OnDisable() {
+            _gameManager.OnDeckDealt -= OnDeckDealt;
+        }
+
+        void OnDeckDealt(IDeck deck) {
+            SetDeck(deck);
+        }
 
         public void SetDeck(IDeck deck) {
             _pile.SetPile(deck.CardPile);
--- Assets/_Project/AppUI/Deck/Scripts/PileHandler.cs
+++ Assets/_Project/AppUI/Deck/Scripts/PileHandler.cs
@@ -1,25 +1,41 @@
 using System.Collections.Generic;
+using _Project.AppUI.Card.Scripts;
+using _Project.AppUI.Components.Draggable.Scripts;
 using _Project.AppUI.Components.Scripts;
 using _Project.Core.Card.Interfaces;
+using Editor.Logger.Scripts;
+using UnityEngine;
 
 namespace _Project.AppUI.Deck.Scripts {
     public class PileHandler : UIButton {
+        [SerializeField] CardHandler _card;
+        [SerializeField] Transform _draggedZone;
+
+        [SerializeField] DraggableContainerBase _container;
         Stack<ICard> _pileCards;
-        
+
         protected override void OnEnable() {
             base.OnEnable();
             OnButtonClicked += DrawCard;
         }
-        
+
         protected override void OnDisable() {
             base.OnDisable();
             OnButtonClicked -= DrawCard;
         }
 
         void DrawCard() {
-            throw new System.NotImplementedException();
+            var card = Instantiate(_card, _draggedZone);
+            card.SetContainer(_container);
+            if (!_pileCards.TryPop(out var cardResult)) {
+                this.LogWarning("Failed to pop card");
+                return;
+            }
+
+            card.SetCardData(cardResult);
+            card.ShowValue = true;
         }
-        
+
         public void SetPile(Stack<ICard> cards) {
             _pileCards = cards;
         }
