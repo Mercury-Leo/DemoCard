f173d861d088cfbf2f40bd272a36116186813501
Mercury <>
2022-12-22 18:25:15 +0200

    IUIDraggable.cs - Added IDrop

--- Assets/_Project/AppUI/Components/Draggable/Interfaces/IUIDraggable.cs
+++ Assets/_Project/AppUI/Components/Draggable/Interfaces/IUIDraggable.cs
@@ -3,11 +3,12 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 
 namespace _Project.AppUI.Components.Draggable.Interfaces {
-    public interface IUIDraggable : IPointerEnterHandler, IDragHandler, IBeginDragHandler, IEndDragHandler, IPointerExitHandler {
-        public abstract Action<Transform> OnObjectEnterHovered { get; set; }
-        public abstract Action OnObjectExitHover { get; set; }
-        public abstract Action<int> OnObjectBeginDrag { get; set; }
-        public abstract Action<Vector3> OnObjectBeingDragged { get; set; }
-        public abstract Action<int> OnObjectEndDrag { get; set; }
+    public interface IUIDraggable : IPointerEnterHandler, IPointerExitHandler, IDragHandler, IBeginDragHandler,
+        IEndDragHandler, IDropHandler {
+        public Action<Transform> OnObjectEnterHovered { get; set; }
+        public Action OnObjectExitHover { get; set; }
+        public Action<Transform> OnObjectBeginDrag { get; set; }
+        public Action<Vector3> OnObjectBeingDragged { get; set; }
+        public Action OnObjectEndDrag { get; set; }
     }
 }
