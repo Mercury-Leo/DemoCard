2bb1f1b2220cbee09b8fa2a759a61a731d05617e
Mercury <>
2022-12-29 21:02:56 +0200

    Updated TurnManager

--- Assets/_Project/Core/TurnManager/Scripts/TurnManager.cs
+++ Assets/_Project/Core/TurnManager/Scripts/TurnManager.cs
@@ -1,46 +1,55 @@
 using System;
 using _Project.Core.Time.Scripts;
+using _Project.Core.TurnManager.Interfaces;
 using UnityEngine;
 using static _Project.Core.CoreConventions;
 
 namespace _Project.Core.TurnManager.Scripts {
     public class TurnManager : MonoBehaviour {
-        readonly TimeManager _timeManager = new(TimerProvider);
+        readonly InternalTimer _timer = new(TimeProvider);
+
+        Coroutine _timerRoutine;
+
+        ITurn _currentTurn;
 
         public Action OnTurnBegin { get; set; }
         public Action OnTurnEnded { get; set; }
         public Action OnTurnLose { get; set; }
 
-        void Awake() {
-            _timeManager.TimeToCount = 60;
-        }
-
         void OnEnable() {
-            _timeManager.OnTimerFinished += OnTimerFinished;
-            _timeManager.OnTimerBegin += OnTimerBegin;
-            _timeManager.OnTimerEnd += OnTimerEnd;
+            _timer.OnTimerFinished += OnTimerFinished;
+            _timer.OnTimerBegin += OnTimerBegin;
+            _timer.OnTimerEnd += OnTimerEnd;
+            _timer.CurrentTime += time => Debug.Log($"current time: {time:F1}");
         }
 
         void OnDisable() {
-            _timeManager.OnTimerFinished -= OnTimerFinished;
-            _timeManager.OnTimerBegin -= OnTimerBegin;
-            _timeManager.OnTimerEnd -= OnTimerEnd;
+            _timer.OnTimerFinished -= OnTimerFinished;
+            _timer.OnTimerBegin -= OnTimerBegin;
+            _timer.OnTimerEnd -= OnTimerEnd;
         }
 
-        void Update() {
-            _timeManager.RunTimer();
-        }
+        public void StartTurn(ITurn turn) {
+            if (turn is null)
+                return;
 
-        public void BeginTurn() {
-            _timeManager.BeginTimer();
+            _currentTurn = turn;
+            _timerRoutine = StartCoroutine(_timer.C_RunTimer(turn.TimeLimit));
         }
 
         public void RestartTurn() {
-            _timeManager.BeginTimer();
+            if (_currentTurn is null)
+                return;
+
+            _timer.AddTime(_currentTurn.TimeLimit, true);
         }
 
-        public void StopTurns() {
-            _timeManager.StopTimer();
+        public void StopTurn() {
+            if (_timerRoutine is null)
+                return;
+
+            StopCoroutine(_timerRoutine);
+            _timer.StopTimer();
         }
 
         void OnTimerFinished() {
