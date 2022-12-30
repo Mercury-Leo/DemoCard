a1555844b0b1036232a83e5dd92bda09a4e770eb
Mercury <>
2022-12-30 14:58:21 +0200

    Added player active

--- Assets/_Project/AppUI/Card/Scripts/CardViewHandler.cs
+++ Assets/_Project/AppUI/Card/Scripts/CardViewHandler.cs
@@ -1,14 +1,18 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using _Project.AppUI.Components.Draggable.Scripts;
+using _Project.AppUI.PlayerID.Scripts;
 using _Project.AppUI.SceneLoaders.CardGame.Scripts;
 using _Project.Core.Card.Interfaces;
+using _Project.Game.Player.Interfaces;
 using Editor.Logger.Scripts;
 using UnityEngine;
 using UnityEngine.UI;
 
 namespace _Project.AppUI.Card.Scripts {
+    [RequireComponent(typeof(PlayerIDHolder))]
     public class CardViewHandler : MonoBehaviour {
         [Header("Components")] [SerializeField]
         GameObject _playerCards;
@@ -17,9 +21,17 @@ namespace _Project.AppUI.Card.Scripts {
 
         [SerializeField] BoundDraggableContainer _container;
 
-        [Header("Prefabs")] [SerializeField] CardGameLoaderSO _loader;
+        [Header("Loader")] [SerializeField] CardGameLoaderSO _loader;
 
         ICard[] _cards;
+
+        IList<CardHandler> _cardHandlers;
+
+        PlayerIDHolder _playerID;
+
+        void Awake() {
+            TryGetComponent(out _playerID);
+        }
 
         void OnValidate() {
             if (_playerCards is null)
@@ -29,7 +41,22 @@ namespace _Project.AppUI.Card.Scripts {
                 this.LogWarning("Layout not assigned", this);
         }
 
-        public void SetPlayerHand(IEnumerable<ICard> cards) {
+        void OnEnable() {
+            _playerID.OnPlayerActive += PlayerActive;
+            _playerID.OnPlayerDeActive += PlayerDeActive;
+        }
+
+        void OnDisable() {
+            _playerID.OnPlayerActive -= PlayerActive;
+            _playerID.OnPlayerDeActive -= PlayerDeActive;
+        }
+
+        public void SetPlayer(IPlayer player) {
+            _playerID.SetID(player.ID);
+            SetPlayerHand(player.GetCards());
+        }
+
+        void SetPlayerHand(IEnumerable<ICard> cards) {
             var enumerable = cards as ICard[] ?? cards.ToArray();
             _cards = enumerable;
             for (var i = 0; i < enumerable.Length; i++) {
@@ -37,16 +64,27 @@ namespace _Project.AppUI.Card.Scripts {
                 prefab.SetCardData(_cards[i]);
                 prefab.OnCardClicked += CardClicked;
                 prefab.SetContainer(_container);
+                _cardHandlers.Add(prefab);
             }
 
             StartCoroutine(DisableLayout());
         }
 
+        void PlayerActive(Guid id) {
+            foreach (var card in _cardHandlers)
+                card.IsPlayerActive = true;
+        }
+
+        void PlayerDeActive(Guid id) {
+            foreach (var card in _cardHandlers)
+                card.IsPlayerActive = false;
+        }
+
+
         IEnumerator DisableLayout() {
             yield return new WaitForEndOfFrame();
             _layout.enabled = false;
         }
-
 
         void CardClicked(ICard card) {
             this.Log($"Clicked card: {card.Value}");
