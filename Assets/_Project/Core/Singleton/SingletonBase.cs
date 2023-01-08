29851b52766e6574664e6a10e52cd1a924e9f381
Mercury <>
2023-01-08 23:42:29 +0200

    Added singleton base

---
+++ Assets/_Project/Core/Singleton/SingletonBase.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace _Project.Core.Singleton {
+    public abstract class SingletonBase<T> : MonoBehaviour where T : MonoBehaviour {
+        static T _instance;
+
+        public static T Instance {
+            get {
+                if (_instance is not null) return _instance;
+                _instance = FindObjectOfType<T>();
+
+                if (_instance is not null) return _instance;
+                var go = new GameObject();
+                _instance = go.AddComponent<T>();
+
+                return _instance;
+            }
+            private set {
+                if (value is null)
+                    return;
+                _instance = value;
+            }
+        }
+
+        protected void Awake() {
+            if (Instance is not null && Instance != this) {
+                Destroy(this);
+                return;
+            }
+            
+            Instance = this as T;
+        }
+    }
+}
