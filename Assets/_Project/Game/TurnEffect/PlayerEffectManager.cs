3a73cd0e8fc791e97d5a39f7b5e34e5a6e519616
Mercury <>
2023-01-10 16:48:46 +0200

    Changed name and removed log

--- Assets/_Project/Game/TurnEffect/PlayerEffectManager.cs
+++ Assets/_Project/Game/TurnEffect/PlayerEffectManager.cs
@@ -1,14 +1,12 @@
 using _Project.Core;
 using _Project.Core.Singleton;
-using Editor.Logger.Scripts;
 
 namespace _Project.Game.TurnEffect {
     public class PlayerEffectManager : SingletonBase<PlayerEffectManager> {
         public CoreConventions.CardEffect CurrentEffect { get; private set; }
 
-        public void SetCurrentTurnEffect(CoreConventions.CardEffect effect) {
+        public void SetCurrentTurnEffect(CoreConventions.CardEffect effect = CoreConventions.CardEffect.NoEffect) {
             CurrentEffect = effect;
-            this.LogSuccess($"Current Effect {CurrentEffect}");
         }
     }
 }
