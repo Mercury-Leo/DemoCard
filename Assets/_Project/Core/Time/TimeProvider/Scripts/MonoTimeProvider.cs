3536908a474d40437619c30e48e47bc7c1e424b9
Mercury <>
2023-02-04 23:33:25 +0200

    Updated namespace

--- Assets/_Project/Core/Time/TimeProvider/Scripts/MonoTimeProvider.cs
+++ Assets/_Project/Core/Time/TimeProvider/Scripts/MonoTimeProvider.cs
@@ -1,6 +1,6 @@
 using _Project.Core.Time.TimeProvider.Interfaces;
 
-namespace _Project.Core.Time.Scripts {
+namespace _Project.Core.Time.TimeProvider.Scripts {
     public class MonoTimeProvider : ITimeProvider {
         public float GetTime() {
             return UnityEngine.Time.deltaTime;
